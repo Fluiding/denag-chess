305e5af11c4dca3ed8a3393901ccca94dd44a4c9
Fluiding <fluiding@proton.me>
2026-01-01 21:08:00 +0100

    multiplayer fix 3

--- scripts/networking.gd
+++ scripts/networking.gd
@@ -1,11 +1,50 @@
 extends Node
 
+@onready var peer = ENetMultiplayerPeer.new()
+var connected = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	multiplayer.peer_connected.connect(on_player_connect)
+	multiplayer.peer_disconnected.connect(on_player_disconnect)
+	multiplayer.connected_to_server.connect(client_connected)
 
+func on_player_connect(_id):
+	if multiplayer.is_server():
+		get_tree().change_scene_to_file("res://scenes/chess_board.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func on_player_disconnect(_id):
 	pass
+
+func host(port: int = 14922):
+	var err = peer.create_server(port, 2)
+	if err:
+		return err
+
+	multiplayer.multiplayer_peer = peer
+	global.is_multiplayer = true
+	global.is_white = true
+
+func client_connected():
+	connected = true
+	global.is_multiplayer = true
+	global.is_white = false
+	get_tree().change_scene_to_file("res://scenes/chess_board.tscn")
+
+func create_client(address: String, port: int = 14922):
+	connected = false
+	if address.is_empty():
+		address = "127.0.0.1"
+	var err = peer.create_client(address, port)
+	return err
+
+func join():
+	multiplayer.multiplayer_peer = peer
+	await get_tree().create_timer(5).timeout
+	if not connected:
+		cancel_connection()
+		return 1
+
+func cancel_connection():
+	multiplayer.multiplayer_peer = null
+	peer.close()
+	peer = ENetMultiplayerPeer.new()
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,12 +1,6 @@
 extends Control
 
 @onready var current_menu = %MainMenu
-@onready var peer = ENetMultiplayerPeer.new()
-
-func _ready() -> void:
-	multiplayer.peer_connected.connect(on_player_connect)
-	multiplayer.peer_disconnected.connect(on_player_disconnect)
-	multiplayer.connected_to_server.connect(client_connected)
 
 func on_exit_pressed():
 	get_tree().quit()
@@ -23,51 +17,27 @@ func singleplayer():
 	global.is_white = bool(randi() % 2)
 	get_tree().change_scene_to_file("res://scenes/chess_board.tscn")
 
-func on_player_connect(_id):
-	if is_inside_tree() and multiplayer.is_server():
-		%HostingMenuContents/StatusLabel.text = "Loading game..."
-		get_tree().change_scene_to_file("res://scenes/chess_board.tscn")
-
-func on_player_disconnect(_id):
-	pass
-
 func host(port: int = 14922):
 	%HostingMenuContents/StatusLabel.text = "Establishing connection..."
-
-	var err = peer.create_server(port, 2)
+	var err = networking.host(port)
 	if err:
 		%HostingMenuContents/StatusLabel.text = "Could not make connection. (error code %s)" % err
 		return err
-
-	multiplayer.multiplayer_peer = peer
-	global.is_multiplayer = true
-	global.is_white = true
 	%HostingMenuContents/StatusLabel.text = "Waiting for opponent..."
-
-func client_connected():
-	global.is_multiplayer = true
-	global.is_white = false
-	get_tree().change_scene_to_file("res://scenes/chess_board.tscn")
 
 func join(port: int = 14922):
 	%JoiningMenuContents/StatusLabel.text = "Establishing connection..."
 
 	var address = %JoinMenuContents/AddressInput.text
-	if address.is_empty():
-		address = "127.0.0.1"
-
-	var err = peer.create_client(address, port)
+	var err = networking.create_client(address, port)
 	if err:
 		%JoiningMenuContents/StatusLabel.text = "Could not make connection. (error code %s)" % err
 		return err
 
 	%JoiningMenuContents/StatusLabel.text = "Searching for server..."
-	multiplayer.multiplayer_peer = peer
-	await get_tree().create_timer(5).timeout
+	err = await networking.join()
 	%JoiningMenuContents/StatusLabel.text = "Could not find server."
 	cancel_connection()
 
 func cancel_connection():
-	multiplayer.multiplayer_peer = null
-	peer.close()
-	peer = ENetMultiplayerPeer.new()
+	networking.cancel_connection()
